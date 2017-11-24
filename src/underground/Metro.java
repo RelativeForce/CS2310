006d055bcef0d92b454c0d0585d67b3e14d8d275
bergj2 <bergj2@aston.ac.uk>
2017-11-24 11:47:45 +0000

    Changed consturctor to only take one parameter, and create a reverse lookup map fro stations to lines

--- src/underground/Metro.java
+++ src/underground/Metro.java
@@ -1,19 +1,34 @@
 package underground;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Metro {
 	
-	private Set<Station> allStations;
+	private final Set<Station> allStations;
+	private final Map<Station, Set<Line>> stationLineLookUp;
+	private final Map<String, Line> lines;
 	
-	private Map<String, Line> lines;
-	
-	public Metro(Set<Station> allStation, Map<String, Line> lines) {
-		this.allStations = allStation;
+	public Metro(Map<String, Line> lines) {
+		final Set<Station> stations = new HashSet<>();
+		final Map<Station, Set<Line>> stationToLine = new HashMap<>();
+		for(final Map.Entry<String, Line> e: lines.entrySet())
+		{
+			final Set<Station> stationsInLine = e.getValue().getStations();
+			stations.addAll(stationsInLine);
+			for(Station station: stationsInLine)
+			{
+				if(!stationToLine.containsKey(station))
+					stationToLine.put(station, new HashSet<>());
+				stationToLine.get(station).add(e.getValue());
+			}
+		}
 		this.lines = lines;
+		this.stationLineLookUp = Collections.unmodifiableMap(stationToLine);
+		this.allStations = Collections.unmodifiableSet(stations);
 	}
-	
-	
-
 }
